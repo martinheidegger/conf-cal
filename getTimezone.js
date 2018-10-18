fabc0d7cbc7463ad64a3c9c796c27c24b9ce1fc0
Martin Heidegger <martin.heidegger@gmail.com>
2018-10-18 13:09:56 +0900

    feat: Improved getTimeZone performance and stability

--- getTimezone.js
+++ getTimezone.js
@@ -2,10 +2,9 @@ const fetch = require('isomorphic-fetch')
 const geoTz = require('geo-tz')
 const fse = require('fs-extra')
 const path = require('path')
-const cacheFile = path.join(process.env.HOME, '.conf-cal.cache')
+const CACHE_VERSION = 1
+const cacheFile = path.normalize(`${process.env.HOME}/.conf-cal_${CACHE_VERSION}.cache`)
 
-let cachedData
-let cachedLookup
 const cache = fse.readJSON(cacheFile)
   .catch(e => {
     if (e.code !== 'ENOENT') {
@@ -13,17 +12,10 @@ const cache = fse.readJSON(cacheFile)
     }
     return {}
   })
-  .then(data => {
-    // Storage only for the raw data
-    cachedData = data
-    // Storage for the raw data as well as the loading processes
-    cachedLookup = Object.assign({}, cachedData)
-    return cachedLookup
-  })
 
 let writingRequested
 let writing
-function writeCacheData () {
+function writeCacheData (cachedData) {
   if (!writing) {
     writing = fse.writeFile(cacheFile, JSON.stringify(cachedData, null, 2))
       .catch(e => console.warn(`Can't write cache file to ${cacheFile}: ${e.stack || e}`))
@@ -35,48 +27,46 @@ function writeCacheData () {
   if (!writingRequested) {
     writingRequested = writing.then(() => {
       writingRequested = null
-      return writeCacheData()
+      return writeCacheData(cachedData)
     })
   }
   return writingRequested
 }
 
-function fromCache (googleObjectId) {
-  return cache.then(cacheLookup => cacheLookup[googleObjectId])
-}
+const requestLookup = {}
 
-module.exports = (apiKey, googleObjectId) =>
-  fromCache(googleObjectId)
-    .then(cached => {
-      if (!cached) {
-        cached = fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${googleObjectId}&key=${apiKey}`)
-          .then(response => {
-            if (response.status >= 400) {
-              return Promise.reject(new Error(`[${response.status}] Error while loading url ${response.text()}`))
-            }
-            return response.json()
-          })
-          .then(json => {
-            if (json.status !== 'OK') {
-              return Promise.reject(new Error(`Google API didn't work out: [${json.status}] ${json.error_message}`))
-            }
-            return json.result
-          })
-          .then(obj => {
-            const loc = obj.geometry.location
-            obj.timeZone = geoTz(loc.lat, loc.lng)
-            return obj
-          })
-          .then(obj => {
-            cachedLookup[googleObjectId] = obj
-            cachedData[googleObjectId] = obj
-            return writeCacheData()
-              .then(() => {
-                return obj
-              })
-          })
-        // For the next loading to use the same promise!
-        cachedLookup[googleObjectId] = cached
+module.exports = (apiKey, googleObjectId) => {
+  let request = requestLookup[googleObjectId]
+  if (!request) {
+    request = cache.then(cachedData => {
+      let obj = cachedData[googleObjectId]
+      if (obj) {
+        return obj
       }
-      return cached
+      return fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${googleObjectId}&key=${apiKey}`)
+        .then(response => {
+          if (response.status >= 400) {
+            return Promise.reject(new Error(`[${response.status}] Error while loading url ${response.text()}`))
+          }
+          return response.json()
+        })
+        .then(json => {
+          if (json.status !== 'OK') {
+            return Promise.reject(new Error(`Google API didn't work out: [${json.status}] ${json.error_message}`))
+          }
+          return json.result
+        })
+        .then(obj => {
+          const loc = obj.geometry.location
+          obj.timeZone = geoTz(loc.lat, loc.lng)
+          return obj
+        })
+        .then(obj => {
+          cachedData[googleObjectId] = obj
+          return writeCacheData(cachedData).then(() => obj)
+        })
     })
+    requestLookup[googleObjectId] = request
+  }
+  return request
+}
